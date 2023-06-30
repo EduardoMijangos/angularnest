8579c467c56fe45ee235a7d358ce6b5cf6034864
EduardoMijangos <lalomijafru126@gmail.com>
2023-06-30 07:14:06 -0600

    Conexion de nest, funciona el login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularnest';
+  formlog: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService
+  ) {
+    this.formlog = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  login() {
+    if (this.formlog.valid) {
+      const loginData = {
+        email: this.formlog.value.email,
+        password: this.formlog.value.password
+      };
+  
+      this.auth.login(loginData).subscribe(
+        response => {
+          console.log(response);
+        },
+        error => {
+          if (error.status === 401) {
+            console.log('Contraseña incorrecta');
+          } else {
+            console.error(error);
+          }
+        }
+      );
+    }
+  }
 }
